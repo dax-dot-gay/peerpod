a08d3049e61d58d8dc0ed76b0be69c50bfee32ea
Dax Harris <mvh3875@rit.edu>
2024-09-28 14:17:04 -0400

    swarm setup & basic relay server

--- peerpod/src/node.rs
+++ peerpod/src/node.rs
@@ -1,11 +1,32 @@
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
+use async_channel::{Receiver, Sender};
 use base64::{engine::general_purpose::URL_SAFE, Engine as _};
 use derive_builder::Builder;
-use libp2p::{identity::{Keypair, PublicKey}, Multiaddr, PeerId};
+use libp2p::StreamProtocol;
+use libp2p::{
+    autonat, dcutr, identify,
+    identity::{Keypair, PublicKey},
+    noise, relay, rendezvous,
+    request_response::{self, ProtocolSupport},
+    swarm::NetworkBehaviour,
+    tcp, upnp, yamux, Multiaddr, PeerId, Swarm,
+};
 use serde::{Deserialize, Serialize};
 
-use crate::types::{Error, PodResult};
+use crate::types::{Command, Error, Event, NodeRequest, NodeResponse, PodResult};
+
+#[derive(NetworkBehaviour)]
+struct Behaviour {
+    relay_client: relay::client::Behaviour,
+    request_response: request_response::json::Behaviour<NodeRequest, NodeResponse>,
+    dcutr: dcutr::Behaviour,
+    rendezvous: rendezvous::client::Behaviour,
+    identify: identify::Behaviour,
+    upnp: upnp::tokio::Behaviour,
+    autonat: autonat::Behaviour,
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum PeerNode {
@@ -36,6 +57,12 @@ pub struct Node {
 
     #[builder(setter(skip))]
     pub known_nodes: Arc<Mutex<Vec<KnownNode>>>,
+
+    #[builder(setter(skip))]
+    pub command_sender: Option<Sender<Command>>,
+
+    #[builder(setter(skip))]
+    pub event_receiver: Option<Receiver<Event>>,
 }
 
 impl Node {
@@ -43,27 +70,45 @@ impl Node {
         let keypair = Keypair::from_protobuf_encoding(
             URL_SAFE
                 .decode(info.key.clone())
-                .or_else(|e| Err(Error::Base64DecodingError {error: e.to_string(), contents: info.key.clone()}))?
+                .or_else(|e| {
+                    Err(Error::Base64DecodingError {
+                        error: e.to_string(),
+                        contents: info.key.clone(),
+                    })
+                })?
                 .as_slice(),
-        ).or(Err(Error::KeypairError("Failed to decode keypair from encoded bytes".to_string())))?;
+        )
+        .or(Err(Error::KeypairError(
+            "Failed to decode keypair from encoded bytes".to_string(),
+        )))?;
         Ok(Node {
             key: keypair,
             class: info.class,
             bootstrap: info.bootstrap,
             friendly_name: info.friendly_name,
-            known_nodes: Arc::new(Mutex::new(info.known_nodes))
+            known_nodes: Arc::new(Mutex::new(info.known_nodes)),
+            command_sender: None,
+            event_receiver: None,
         })
     }
 
     pub fn into_info(&self) -> PodResult<NodeInfo> {
-        let encoded_key = URL_SAFE.encode(self.key.to_protobuf_encoding().or(Err(Error::KeypairError("Failed to encode keypair as bytes".to_string())))?);
-        let known = self.known_nodes.lock().or(Err(Error::SyncError("Failed to unwrap known_nodes".to_string())))?.clone();
+        let encoded_key = URL_SAFE.encode(self.key.to_protobuf_encoding().or(Err(
+            Error::KeypairError("Failed to encode keypair as bytes".to_string()),
+        ))?);
+        let known = self
+            .known_nodes
+            .lock()
+            .or(Err(Error::SyncError(
+                "Failed to unwrap known_nodes".to_string(),
+            )))?
+            .clone();
         Ok(NodeInfo {
             key: encoded_key,
             class: self.class.clone(),
             bootstrap: self.bootstrap.clone(),
             friendly_name: self.friendly_name.clone(),
-            known_nodes: known
+            known_nodes: known,
         })
     }
 
@@ -73,6 +118,89 @@ impl Node {
 
     pub fn peer_id(&self) -> PeerId {
         self.key.public().to_peer_id()
+    }
+
+    pub fn add_known(&self, node: KnownNode) -> PodResult<()> {
+        let mut nodes = self.known_nodes.lock().or(Err(Error::SyncError(
+            "Failed to acquire lock on known_nodes".to_string(),
+        )))?;
+        (*nodes).push(node.clone());
+        (*nodes).dedup_by(|a, b| a.id.eq(&b.id));
+        Ok(())
+    }
+
+    pub fn remove_known(&self, id: PeerId) -> PodResult<()> {
+        let mut nodes = self.known_nodes.lock().or(Err(Error::SyncError(
+            "Failed to acquire lock on known_nodes".to_string(),
+        )))?;
+        (*nodes).retain(|v| !v.id.eq(&id));
+        Ok(())
+    }
+
+    pub fn get_known(&self) -> PodResult<Vec<KnownNode>> {
+        let nodes = self.known_nodes.lock().or(Err(Error::SyncError(
+            "Failed to acquire lock on known_nodes".to_string(),
+        )))?;
+        Ok((*nodes).clone())
+    }
+
+    async fn event_loop(
+        &self,
+        swarm: Swarm<Behaviour>,
+        commands: Receiver<Command>,
+        events: Sender<Event>,
+    ) -> PodResult<()> {
+        Ok(())
+    }
+
+    pub fn initialize(&mut self) -> PodResult<()> {
+        let mut swarm = libp2p::SwarmBuilder::with_existing_identity(self.key.clone())
+            .with_tokio()
+            .with_tcp(
+                tcp::Config::default(),
+                noise::Config::new,
+                yamux::Config::default,
+            )
+            .or_else(|e| {
+                Err(Error::SwarmError {
+                    kind: crate::types::SwarmErrorType::Initialization,
+                    reason: e.to_string(),
+                })
+            })?
+            .with_relay_client(noise::Config::new, yamux::Config::default)
+            .or_else(|e| {
+                Err(Error::SwarmError {
+                    kind: crate::types::SwarmErrorType::RelaySetup,
+                    reason: e.to_string(),
+                })
+            })?
+            .with_behaviour(|key, relay_behaviour| Behaviour {
+                relay_client: relay_behaviour,
+                identify: identify::Behaviour::new(identify::Config::new(
+                    format!("peerpod/{}", self.class),
+                    key.public(),
+                )),
+                request_response: request_response::json::Behaviour::new(
+                    [(StreamProtocol::new("/peerpod-json"), ProtocolSupport::Full)],
+                    request_response::Config::default(),
+                ),
+                dcutr: dcutr::Behaviour::new(key.public().to_peer_id()),
+                rendezvous: rendezvous::client::Behaviour::new(key.clone()),
+                upnp: upnp::tokio::Behaviour::default(),
+                autonat: autonat::Behaviour::new(
+                    key.public().to_peer_id(),
+                    autonat::Config::default(),
+                ),
+            })
+            .or_else(|e| {
+                Err(Error::SwarmError {
+                    kind: crate::types::SwarmErrorType::BehaviourSetup,
+                    reason: e.to_string(),
+                })
+            })?
+            .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
+            .build();
+        Ok(())
     }
 }
 
--- peerpod/src/types.rs
+++ peerpod/src/types.rs
@@ -1,13 +1,26 @@
 use std::fmt::Display;
 
-use serde::{Deserialize, Serialize};
+use libp2p::PeerId;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde_json::Value;
+use uuid::Uuid;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum SwarmErrorType {
+    Initialization,
+    RelaySetup,
+    BehaviourSetup
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum Error {
-    Base64EncodingError{error: String, contents: Vec<u8>},
-    Base64DecodingError{error: String, contents: String},
+    Base64EncodingError { error: String, contents: Vec<u8> },
+    Base64DecodingError { error: String, contents: String },
     KeypairError(String),
-    SyncError(String)
+    SyncError(String),
+    JsonDecodingError { error: String, contents: Value },
+    JsonEncodingError(String),
+    SwarmError{kind: SwarmErrorType, reason: String}
 }
 
 impl Display for Error {
@@ -17,3 +30,66 @@ impl Display for Error {
 }
 
 pub type PodResult<T> = Result<T, Error>;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum Command {}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum Event {}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub struct NodeRequest {
+    pub id: Uuid,
+    pub source: PeerId,
+    pub content: Value,
+}
+
+impl NodeRequest {
+    pub fn unwrap<T: Serialize + DeserializeOwned>(&self) -> PodResult<T> {
+        Ok(
+            serde_json::from_value::<T>(self.content.clone()).or_else(|e| {
+                Err(Error::JsonDecodingError {
+                    error: e.to_string(),
+                    contents: self.content.clone(),
+                })
+            })?,
+        )
+    }
+
+    pub fn new<T: Serialize + DeserializeOwned>(
+        source: PeerId,
+        content: T,
+    ) -> PodResult<NodeRequest> {
+        Ok(NodeRequest {
+            id: Uuid::new_v4(),
+            source: source.clone(),
+            content: serde_json::to_value(content)
+                .or_else(|e| Err(Error::JsonEncodingError(e.to_string())))?,
+        })
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub struct NodeResponse {
+    pub request_id: Uuid,
+    pub content: Result<Value, Value>,
+}
+
+impl NodeResponse {
+    pub fn respond<T: Serialize + DeserializeOwned, E: Serialize + DeserializeOwned>(
+        request: NodeRequest,
+        result: Result<T, E>,
+    ) -> PodResult<NodeResponse> {
+        let encoded_result = match result {
+            Ok(t) => Ok(serde_json::to_value(t)
+                .or_else(|e| Err(Error::JsonEncodingError(e.to_string())))?),
+            Err(e) => Err(serde_json::to_value(e)
+                .or_else(|e| Err(Error::JsonEncodingError(e.to_string())))?),
+        };
+
+        Ok(NodeResponse {
+            request_id: request.id.clone(),
+            content: encoded_result,
+        })
+    }
+}
